eea005a68f7c12fa6df5b928e668829d30e0e8e0
tripur5348 <tripurmuley5348@gmail.com>
2024-12-18 22:17:22 +0530

    Second commit

---
+++ PortfolioApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
