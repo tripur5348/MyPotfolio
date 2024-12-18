c77b63597bf44960c53999a00c633ee908c17406
tripur5348 <tripurmuley5348@gmail.com>
2024-12-18 23:01:52 +0530

    3rd commit

--- PortfolioApp/obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
+++ PortfolioApp/obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea005a68f7c12fa6df5b928e668829d30e0e8e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
